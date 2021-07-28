29c528cf10fd624d93e038d0455a41bdc62b9caf
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-28 19:23:04 +0200

    fine

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,7 @@ console.log("Ciao", Vue);
 const app = new Vue({
   el: "#app",
   data: {
+    currentIndex: 0,
     images: [
       "images/image1.jpg",
       "images/image2.jpg",
@@ -11,5 +12,29 @@ const app = new Vue({
       "images/image4.jpg",
     ],
   },
-  methods: {},
+  methods: {
+    isActive(index) {
+      return this.currentIndex === index ? "active" : "";
+    },
+    incremento() {
+      const lastIndex = this.images.length - 1;
+      if (this.currentIndex === lastIndex) {
+        this.currentIndex = 0;
+      } else {
+        this.currentIndex++;
+      }
+    },
+    decremento() {
+      const lastIndex = this.images.length - 1;
+      if (this.currentIndex === 0) {
+        this.currentIndex = lastIndex;
+      } else {
+        this.currentIndex--;
+      }
+    },
+    setCurrentIndex(newIndex) {
+      this.currentIndex = newIndex;
+    },
+  },
 });
+var myVar;
