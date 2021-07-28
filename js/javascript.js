049132a3dd8fcbcc7e3cd453ec827b1701991c39
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-28 17:00:54 +0200

    inserisco le immagini

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,13 @@ console.log("Ciao", Vue);
 
 const app = new Vue({
   el: "#app",
-  date: {},
+  data: {
+    images: [
+      "images/image1.jpg",
+      "images/image2.jpg",
+      "images/image3.jpg",
+      "images/image4.jpg",
+    ],
+  },
   methods: {},
 });
